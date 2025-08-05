7c88b205dab77ec3fab9cf8984879fa28334145b
Yeonju Jo <yjj0287@gmail.com>
2025-08-05 11:25:00 +0900

    feat: 데이터베이스 설정 및 JPA 구성 추가, 환경 설정 파일 변경

---
+++ src/main/java/com/juju/realtime/infrastructure/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.juju.realtime.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.juju.realtime.infrastructure.persistence")
+public class JpaConfig {
+} 
